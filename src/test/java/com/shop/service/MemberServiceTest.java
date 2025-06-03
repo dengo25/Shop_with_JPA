c627ca72f799fe8ddf4e5b5c5be1df9a7de10531
dengo23-dev <dengo23.dev@gmail.com>
2025-06-03 09:33:38 +0900

    [Feat] MemberController 생성 && MemberForm 생성

--- src/test/java/com/shop/service/MemberServiceTest.java
+++ src/test/java/com/shop/service/MemberServiceTest.java
@@ -39,5 +39,23 @@ class MemberServiceTest {
     Member savedMember = memberService.saveMember(member);
     
     assertEquals(member.getEmail(), savedMember.getEmail());
+    assertEquals(member.getName(), savedMember.getName());
+    assertEquals(member.getAddress(), savedMember.getAddress());
+    assertEquals(member.getPassword(), savedMember.getPassword());
+    assertEquals(member.getRole(), savedMember.getRole());
+  }
+  
+  @Test
+  @DisplayName("중복 회원 가입 테스트")
+  public void saveDuplicateMemberTest() {
+    Member member1 = createMember();
+    Member member2 = createMember();
+    memberService.saveMember(member1);
+    
+    Throwable e = assertThrows(IllegalStateException.class, () -> {
+      memberService.saveMember(member2);
+    });
+    assertEquals("이미 가입된 회원입니다.", e.getMessage());
+    
   }
 }
