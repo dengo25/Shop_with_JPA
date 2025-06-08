b08c5f1dbc8f0fb8f73d141580afbafe2689b3da
dengo23-dev <dengo23.dev@gmail.com>
2025-06-08 14:56:24 +0900

    [Feat] Order, OrderItem 영속성 전이 테스트

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -29,7 +29,7 @@ public class Order {
   @Enumerated(EnumType.STRING)
   private OrderStatus orderStatus; //주문 상태
   
-  @OneToMany(mappedBy = "order") //연관관계의 주인이 order
+  @OneToMany(mappedBy = "order",cascade = CascadeType.ALL) //연관관계의 주인이 order,자식엔티티에도 영송성 상태 모두 전이
   private List<OrderItem> orderItems = new ArrayList<>();
   
   private LocalDateTime refTime;
---
+++ src/test/java/com/shop/repository/OrderRepositoryTest.java
@@ -0,0 +1,75 @@
+package com.shop.repository;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.entity.Item;
+import com.shop.entity.Order;
+import com.shop.entity.OrderItem;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties") //테스트용으로 h2사용
+class OrderRepositoryTest {
+  
+  @Autowired
+  OrderRepository orderRepository;
+  
+  @Autowired
+  ItemRepository itemRepository;
+  
+  @PersistenceContext
+  EntityManager em;
+  
+  public Item createItem() {
+    Item item = new Item();
+    item.setItemNm("테스트 상품");
+    item.setPrice(10000);
+    item.setItemDetail("테스트 상품 상세 설명");
+    item.setItemSellStatus(ItemSellStatus.SELL);
+    item.setStockNumber(100);
+    item.setRegTime(LocalDateTime.now());
+    item.setUpdateTime(LocalDateTime.now());
+    return item;
+    
+  }
+  
+  @Test
+  @DisplayName("영속성 전이 테스트")
+  public void cascadeTest() {
+    Order order = new Order();
+    for (int i = 0; i < 3; i++) {
+      Item item = this.createItem();
+      itemRepository.save(item);
+      OrderItem orderItem = new OrderItem();
+      orderItem.setItem(item);
+      orderItem.setCount(10);
+      orderItem.setOrderPrice(10000);
+      orderItem.setOrder(order);
+      order.getOrderItems().add(orderItem);
+    }
+    orderRepository.saveAndFlush(order); //여기서 order를 저장할 때 OrderItem도 저장됨
+    em.clear();  //영속성 컨텍스트초기화
+    
+    
+    Order savedOrder = orderRepository.findById(order.getId())
+        .orElseThrow(EntityNotFoundException::new);
+    Assertions.assertEquals(3, savedOrder.getOrderItems().size());
+  }
+  
+  
+  
+}
